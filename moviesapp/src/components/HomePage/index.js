de6a66b4326167d28440af9dc6160de50c22ccdb
KhaledAmmer <k.tayseer.amer@gmail.com>
2022-07-05 22:12:39 +0300

    working on homePage 4

--- moviesapp/src/components/HomePage/index.js
+++ moviesapp/src/components/HomePage/index.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { Movies } from "../../controllers/movie";
 import { Button, Card } from "react-bootstrap";
 import { Info } from "../../controllers/info";
-import "./style.css"
+import "./style.css";
 export const HomePage = () => {
   const [movies, setMovies] = useState([]);
 
@@ -14,16 +14,23 @@ export const HomePage = () => {
     })();
   }, []);
   const buildMovieCard = (movie) => {
-     return (
+    return (
       <Card>
         <Card.Img variant="top" src={`${Info.imagesUrl + movie.poster_path}`} />
         <Card.Body>
-          <Card.Title>{movie.title}</Card.Title>
-          <h6>{movie.release_date}</h6>
-          {movie.genre_ids.length?movie.genre_ids.map(genre=>{
-            return  <small key={genre + movie.id}>{genre+", "}</small>
-          }):<></>}
-         
+          <div className="card-details-div">
+            <Card.Title>{movie.title}</Card.Title>
+            <h6>{movie.release_date}</h6>
+            <div>
+              {movie.genre_ids.length ? (
+                movie.genre_ids.map((genre) => {
+                  return <small key={genre + movie.id}>{genre + ", "}</small>;
+                })
+              ) : (
+                <></>
+              )}
+            </div>
+          </div>
         </Card.Body>
       </Card>
     );
@@ -32,14 +39,14 @@ export const HomePage = () => {
     <div className="main-home-page-div">
       <div className="cards-div">
         <h1>Action</h1>
-        <div  className="inner-cards-div">
-        {movies.length ? (
-          movies.map((movie) => {
-            return buildMovieCard(movie);
-          })
-        ) : (
-          <></>
-        )}
+        <div className="inner-cards-div">
+          {movies.length ? (
+            movies.map((movie) => {
+              return buildMovieCard(movie);
+            })
+          ) : (
+            <></>
+          )}
         </div>
       </div>
     </div>
